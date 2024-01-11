f1415251dee1ab52f1f00354b2aa31518d2a06e9
Rachel <yyxxcairunxin@sina.cn>
2024-01-11 22:36:00 +0800

    change domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_web_forum", domain: "http://localhost:3000"
+    Rails.application.config.session_store :cookie_store, key: "_web_forum", domain: "https://pacific-headland-38946-b42f15bc43e4.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_web_forum"
 end
