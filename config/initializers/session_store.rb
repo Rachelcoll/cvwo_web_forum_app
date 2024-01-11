1b07f9428a8fc81e67a6daf5d4c7e1987b75d6c8
Rachel <yyxxcairunxin@sina.cn>
2024-01-11 22:49:51 +0800

    heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_web_forum", domain: "https://pacific-headland-38946-b42f15bc43e4.herokuapp.com"
-else
-    Rails.application.config.session_store :cookie_store, key: "_web_forum"
-end
+Rails.application.config.session_store :cookie_store, key: "_web_forum", domain: :all, secure: Rails.env.production?, same_site: :lax
