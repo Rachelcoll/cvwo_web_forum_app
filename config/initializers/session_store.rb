a3edeaf7bd03c7126bc2df2b059de0ac1d151a5d
Rachel <yyxxcairunxin@sina.cn>
2024-01-12 12:45:16 +0800

    session-store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_web_forum", domain: :all
+Rails.application.config.session_store :cookie_store, key: "_web_forum", domain: "https://pacific-headland-38946-b42f15bc43e4.herokuapp.com/"
