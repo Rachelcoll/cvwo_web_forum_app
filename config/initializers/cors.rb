2613945380ed87be3aec3c667e1efc9499ffed88
Rachel <yyxxcairunxin@sina.cn>
2024-01-18 22:09:02 +0800

    netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    
     allow do
-        origins "http://localhost:3000"
+        origins "https://main--comforting-starburst-cdbfae.netlify.app/" #replace this with final react website
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+
     allow do
-        origins "http://localhost:3001" #replace this with final react website
+        origins "http://localhost:3000"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
